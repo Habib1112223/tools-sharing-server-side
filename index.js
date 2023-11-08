1c2ddad4fcd7d003f78a4660ab9b40a92aa33861
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2023-11-08 19:02:02 +0600

    cors app set up in the github

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// Middleware
+app.use(cors());
